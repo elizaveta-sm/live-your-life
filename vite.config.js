b964cb34304dc46e3019d694ace42af8fbaaeccf
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-03-17 10:08:21 +0300

    new changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_REACT_APP_SERVERURL,
+  base: import.meta.env.VITE_REACT_APP_SERVERURL,
 })
