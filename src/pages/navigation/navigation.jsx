693f504c6c387f13790b9f180b8cadff3b1f2c10
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-03-19 16:24:38 +0300

    trying to solve 'cannot read properties of undefined'

--- src/pages/navigation/navigation.jsx
+++ src/pages/navigation/navigation.jsx
@@ -12,13 +12,11 @@ const Navigation = () => {
 
     console.log('cookies: ', cookies);
     
-    const userEmail = (cookies.Email !== 'undefined') ? cookies.Email : '';
-    const userName = (cookies.Username !== 'undefined') ? cookies.Username : '';
-    const imageUrl = (cookies.ImageUrl !== 'undefined') ? cookies.ImageUrl : '';
+    const userEmail = (cookies.Email !== undefined) ? cookies.Email : '';
+    const userName = (cookies.Username !== undefined) ? cookies.Username : '';
+    const imageUrl = (cookies.ImageUrl !== undefined) ? cookies.ImageUrl : '';
 
-    console.log({userName})
     console.log({userEmail})
-    console.log({imageUrl})
 
     return (
       <>
