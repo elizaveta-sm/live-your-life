e7f12013acbd49551bead72483f8624b3766abb8
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-03-19 18:32:46 +0300

    solving the cookies problem

--- src/pages/navigation/navigation.jsx
+++ src/pages/navigation/navigation.jsx
@@ -23,7 +23,7 @@ const Navigation = () => {
     const userName = isUndefined(cookies.Username) ? '' : cookies.Username;
     const imageUrl = isUndefined(cookies.ImageUrl) ? '' : cookies.ImageUrl;
 
-    console.log('user email in the cookies in the navigation: ', userEmail);
+    console.log('user email in the cookies in the navigation: ', userEmail)
 
     return (
       <>
@@ -65,15 +65,6 @@ const Navigation = () => {
 
                 {
                   userName.length && userEmail.length ? (
-                    <NavLink className="hs-collapse-toggle flex items-center gap-x-2 font-medium text-gray-500 hover:underline lg:border-l lg:border-gray-300 lg:my-6 lg:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:underline" data-hs-collapse="#navbar-collapse-with-animation" to='/login' style={({isActive}) => ({
-                      color: isActive ? '#a855f7' : '#64748b'
-                    })} >
-                      <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
-                      </svg>
-                      Login
-                    </NavLink>
-                  ) : (
                     <>
                       <NavLink className="hs-collapse-toggle flex items-center gap-x-2 font-medium text-gray-500 hover:underline lg:border-l lg:border-gray-300 lg:my-6 lg:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:underline" data-hs-collapse="#navbar-collapse-with-animation" to='/update-profile' style={({isActive}) => ({
                         color: isActive ? '#a855f7' : '#64748b'
@@ -91,6 +82,15 @@ const Navigation = () => {
                         Logout
                       </NavLink>
                     </>
+                  ) : (
+                    <NavLink className="hs-collapse-toggle flex items-center gap-x-2 font-medium text-gray-500 hover:underline lg:border-l lg:border-gray-300 lg:my-6 lg:pl-6 dark:border-gray-700 dark:text-gray-400 dark:hover:underline" data-hs-collapse="#navbar-collapse-with-animation" to='/login' style={({isActive}) => ({
+                      color: isActive ? '#a855f7' : '#64748b'
+                    })} >
+                      <svg className="w-4 h-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                        <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"/>
+                      </svg>
+                      Login
+                    </NavLink>
                   )
                 }
 
