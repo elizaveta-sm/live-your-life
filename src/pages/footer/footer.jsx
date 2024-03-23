46caddde9504bd4fead23f08bfbcd4f7f5c0fdcd
Elizaveta S <elizaveta.s.webdev@gmail.com>
2024-03-23 15:26:00 +0300

    new changes

--- src/pages/footer/footer.jsx
+++ src/pages/footer/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
       <hr className="mb-8 border-blue-gray-50" />
       
       <div className="text-center">
-        <img src='/assets/blog-app-logo.png' alt="Live Your Life logo" className='h-14 w-14 place-self-center' />
+        <img src='/assets/blog-app-logo.png' alt="Live Your Life logo" className='h-20 w-20 flex place-self-center' />
 
         <div>
           <a className="flex-none text-xl font-semibold text-black dark:text-white" href="#" aria-label="Brand">Live Your Life</a>
